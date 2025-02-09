83b2fdb20f22db5c33e8c9cabe8fc5063015042a
Kelvin Adithya <72134234+kelvinkn17@users.noreply.github.com>
2025-02-09 22:31:40 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,27 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
 import { fileURLToPath } from "url";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    nodePolyfills({
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
