fc7cf9a3bbe88bedd17d1474b477560b7eb1bf86
Kelvin Adithya <72134234+kelvinkn17@users.noreply.github.com>
2025-02-09 07:34:14 +0700

    /login

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -23,7 +23,7 @@ export default function AppLayout() {
         },
         loginMethods: [
           "email",
-          "google"
+          // "google"
         ],
         fundingMethodConfig: {
           moonpay: {
