ee7264c2ed6ef231fe7bc0672a9b6cb2461d74ef
Kelvin Adithya <72134234+kelvinkn17@users.noreply.github.com>
2025-02-09 07:38:39 +0700

    Update authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,34 +4,43 @@ import { prismaQuery } from '../../lib/prisma.js';
 import { privy } from '../../lib/privy.js';
 
 export const authMiddleware = async (request, reply) => {
-  const token = request.headers.authorization.split(' ')[1];
-  console.log('Token:', token);
+  // const token = request.headers.authorization.split(' ')[1];
+  // console.log('Token:', token);
 
-  let authData = null;
-  try {
-    const verifiedClaims = await privy.verifyAuthToken(token);
-    console.log('Verified claims:', verifiedClaims);
+  // let authData = null;
+  // try {
+  //   const verifiedClaims = await privy.verifyAuthToken(token);
+  //   console.log('Verified claims:', verifiedClaims);
 
-    authData = verifiedClaims;
-  } catch (error) {
-    console.log(`Token verification failed with error ${error}.`);
-    return reply.code(401).send({
-      error: 'Invalid token'
-    });
-  }
+  //   authData = verifiedClaims;
+  // } catch (error) {
+  //   console.log(`Token verification failed with error ${error}.`);
+  //   return reply.code(401).send({
+  //     error: 'Invalid token'
+  //   });
+  // }
+
+  // const user = await prismaQuery.user.findUnique({
+  //   where: {
+  //     id: authData.userId
+  //   }
+  // })
+
+  // if (!user) {
+  //   reply.code(401).send({
+  //     error: 'Unauthorized'
+  //   });
+  //   return false;
+  // }
+
+  // request.user = user;
+  // return true;
 
   const user = await prismaQuery.user.findUnique({
     where: {
-      id: authData.userId
+      email: 'testing@testing.com'
     }
   })
-
-  if (!user) {
-    reply.code(401).send({
-      error: 'Unauthorized'
-    });
-    return false;
-  }
 
   request.user = user;
   return true;
